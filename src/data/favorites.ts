acd4c198877f20e375d00edee1b60d95d4f24eb4
YuKirasawa <taianqiyuxuan@gmail.com>
2026-08-02 19:19:08 +0800

    update

--- src/data/favorites.ts
+++ src/data/favorites.ts
@@ -13,4 +13,11 @@ export const favorites: Favorite[] = [
 		url: 'https://forgpt-d0g49jg3794cda582-1456017603.tcloudbaseapp.com/',
 		tags: ['金融'],
 	},
+	{
+		title: 'hypeliquid 回购',
+		description:
+			'hypeliquid 代币经济学（收入/回购/供应量）看板',
+		url: 'https://hype-data-worker.yukirasawa.workers.dev/',
+		tags: ['金融'],
+	},
 ];
